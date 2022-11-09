c0d4e9e3a01d74e6c1c952cf89a28316d4e43753
amaangigani7 <amaangigani35@gmail.com>
2022-11-09 19:00:48 +0530

    secret removed

--- vec_api/settings.py
+++ vec_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jx4!p2(1p)8n01xf_*0-54jh737u@d$m_pyb&dbyx0#(tbr#^m"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
